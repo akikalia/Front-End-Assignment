808193fdbb32ad4b608eafb943cca9d850c01446
Alexander Kikalia <47707757+akikalia@users.noreply.github.com>
2023-03-02 17:52:30 +0400

    Delete static directory

--- static/js/search.js
+++
@@ -1,8 +0,0 @@
-
-
-let searchForm = document.querySelector("#search-form");
-
-searchForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    location.hash = "search/" + document.querySelector("#search-value").value;
-});
